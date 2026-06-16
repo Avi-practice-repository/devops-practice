215ddb12bda7dfb1b3e808221f7804c3eeaceee9
Avi-practice-repository <avinashrahigude25@gmail.com>
2026-06-16 16:12:42 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,12 @@
+color=("red", "green", "blue","pink", "yellow")
+print(color[0])
+print(color[-1])
+print(color[-2])
+
+print(len(color))
+
+print(color.count(2))
+print(color.index(3))
+color.append("black")
+
+
